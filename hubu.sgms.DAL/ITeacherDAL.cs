870dcd3d3c0e22bb1908639a617a30fdd5b8410b
zks <119801760@qq.com>
2017-10-24 23:03:45 +0800

    加了点点

--- hubu.sgms.DAL/ITeacherDAL.cs
+++ hubu.sgms.DAL/ITeacherDAL.cs
@@ -9,6 +9,20 @@ namespace hubu.sgms.DAL
 {
     public interface ITeacherDAL
     {
+        /// <summary>
+        /// 查询所有的课程信息
+        /// </summary>
+        /// <param name="department"></param>
+        /// <param name="major"></param>
+        /// <returns></returns>
         IList<Teacher_course> SelectAllCourse(string department, string major);
+
+        /// <summary>
+        /// 查询所有选课信息   
+        /// </summary>
+        /// <param name="department"></param>
+        /// <param name="major"></param>
+        /// <returns></returns>
+        IList<Course_choosing> SelectAllStudentCourse(string department, string major);
     }
 }
